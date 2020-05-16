a9c0827a0d3b90d9fbf8e361d4191598e1e77843
nikita <sna2021@gmail.com>
2020-05-16 04:11:48 +0300

    - no message

--- permission_handler/ios/Classes/PermissionManager.m
+++ permission_handler/ios/Classes/PermissionManager.m
@@ -75,12 +75,8 @@
 
 + (id)createPermissionStrategy:(PermissionGroup)permission {
     switch (permission) {
-        case PermissionGroupCalendar:
-            return [EventPermissionStrategy new];
         case PermissionGroupCamera:
             return [AudioVideoPermissionStrategy new];
-        case PermissionGroupContacts:
-            return [ContactPermissionStrategy new];
         case PermissionGroupLocation:
         case PermissionGroupLocationAlways:
         case PermissionGroupLocationWhenInUse:
@@ -89,27 +85,48 @@
             #else
             return [LocationPermissionStrategy new];
             #endif
-        case PermissionGroupMediaLibrary:
-            return [MediaLibraryPermissionStrategy new];
-        case PermissionGroupMicrophone:
-            return [AudioVideoPermissionStrategy new];
-        case PermissionGroupPhone:
-            return [PhonePermissionStrategy new];
-        case PermissionGroupPhotos:
-            return [PhotoPermissionStrategy new];
-        case PermissionGroupReminders:
-            return [EventPermissionStrategy new];
-        case PermissionGroupSensors:
-            return [SensorPermissionStrategy new];
-        case PermissionGroupSpeech:
-            return [SpeechPermissionStrategy new];
-        case PermissionGroupNotification:
-            return [NotificationPermissionStrategy new];
-        case PermissionGroupStorage:
-            return [StoragePermissionStrategy new];
         default:
             return [UnknownPermissionStrategy new];
     }
 }
+
+//+ (id)createPermissionStrategy:(PermissionGroup)permission {
+//    switch (permission) {
+//        case PermissionGroupCalendar:
+//            return [EventPermissionStrategy new];
+//        case PermissionGroupCamera:
+//            return [AudioVideoPermissionStrategy new];
+//        case PermissionGroupContacts:
+//            return [ContactPermissionStrategy new];
+//        case PermissionGroupLocation:
+//        case PermissionGroupLocationAlways:
+//        case PermissionGroupLocationWhenInUse:
+//            #if PERMISSION_LOCATION
+//            return [[LocationPermissionStrategy alloc] initWithLocationManager];
+//            #else
+//            return [LocationPermissionStrategy new];
+//            #endif
+//        case PermissionGroupMediaLibrary:
+//            return [MediaLibraryPermissionStrategy new];
+//        case PermissionGroupMicrophone:
+//            return [AudioVideoPermissionStrategy new];
+//        case PermissionGroupPhone:
+//            return [PhonePermissionStrategy new];
+//        case PermissionGroupPhotos:
+//            return [PhotoPermissionStrategy new];
+//        case PermissionGroupReminders:
+//            return [EventPermissionStrategy new];
+//        case PermissionGroupSensors:
+//            return [SensorPermissionStrategy new];
+//        case PermissionGroupSpeech:
+//            return [SpeechPermissionStrategy new];
+//        case PermissionGroupNotification:
+//            return [NotificationPermissionStrategy new];
+//        case PermissionGroupStorage:
+//            return [StoragePermissionStrategy new];
+//        default:
+//            return [UnknownPermissionStrategy new];
+//    }
+//}
 
 @end
